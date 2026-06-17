78182404b9594291c99f873d96a067dd045e6841
Rose Atkinson <roseatkinson@sysopss-MacBook-Air.local>
2026-06-17 11:52:47 +0100

    beginning changeover to scipy hists, adding std code

--- Plotting_Notebooks/heatmap_modules.py
+++ Plotting_Notebooks/heatmap_modules.py
@@ -1,65 +1,51 @@
 import numpy as np 
 import pandas as pd
+from scipy import stats
 
 ##################################################################
 
 def compute_hists2d(df):
-    """Compute 2D heatmaps of power and compressibilty, ignoring bins with <50 intervals"""
+    """Compute 2D heatmaps of transverse and compressive power and compressibilty, ignoring bins with <50 intervals"""
     x_col='GIPM X (OMNI mean)'
     y_col='GIPM Y (OMNI mean)'
-    w_compressive='ULF Band Normalised Compressive Power'
     w_transverse='ULF Band Normalised Transverse Power'
+    w_compressive='ULF Band Normalised Compressive Power'
     w_compressible='Compressibility'
-    
-    hist, _, _ = np.histogram2d(
-        df[x_col].to_numpy(),
-        df[y_col].to_numpy(),
-        bins=[x_bin_edges, y_bin_edges]
-    )
-    hist = hist.T
-    hist[hist == 0] = np.nan
 
+    hist_count,x_edge, y_edge, _ = stats.binned_statistic_2d(df[x_col].to_numpy(), df[y_col].to_numpy(), df[w_transverse].to_numpy(), statistic='count', bins=[x_bin_edges, y_bin_edges])
+    
     #produce a copy of count distribution histogram for masking purposes
-    hist_count = hist.copy()
-    hist_count[hist_count < 50] = np.nan
+    hist_count_c = hist.copy()
+    hist_count_c[hist_count < 50] = np.nan
 
-    #compressive power histogram
-    hist_comp, _, _ = np.histogram2d(
-        df[x_col].to_numpy(),
-        df[y_col].to_numpy(),
-        bins=[x_bin_edges, y_bin_edges],
-        weights=df[w_compressive].to_numpy()
-    )
-    hist_comp = hist_comp.T
-    hist_comp[hist_comp == 0] = np.nan
-    #normalise to find averages
-    hist_comp = hist_comp/hist_count
-
-    #transverse power histogram
-    hist_trans, _, _ = np.histogram2d(
-        df[x_col].to_numpy(),
-        df[y_col].to_numpy(),
-        bins=[x_bin_edges, y_bin_edges],
-        weights=df[w_transverse].to_numpy()
-    )
+    #transverse
+    hist_trans,x_edge, y_edge, _ = stats.binned_statistic_2d(df[x_col].to_numpy(), df[y_col].to_numpy(), df[w_transverse].to_numpy(), statistic='mean', bins=[x_bin_edges, y_bin_edges])
+    hist_trans = np.where(np.isnan(hist_count_c), np.nan, hist_trans)
     hist_trans = hist_trans.T
-    hist_trans[hist_trans == 0] = np.nan
-    #normalise to find averages
-    hist_trans = hist_trans/hist_count
 
-    #compressibility histogram
-    hist_compressibility, _, _ = np.histogram2d(
-        df[x_col].to_numpy(),
-        df[y_col].to_numpy(),
-        bins=[x_bin_edges, y_bin_edges],
-        weights=df[w_compressible].to_numpy()
-    )
-    hist_compressibility = hist_compressibility.T
-    hist_compressibility[hist_compressibility == 0] = np.nan
-    #normalise to find averages
-    hist_compressibility = hist_compressibility/hist_count
+    #transverse std
+    hist_trans_std,x_edge, y_edge, _ = stats.binned_statistic_2d(df[x_col].to_numpy(), df[y_col].to_numpy(), df[w_transverse].to_numpy(), statistic='std', bins=[x_bin_edges, y_bin_edges])
+    hist_trans_std = np.where(np.isnan(hist_count_c), np.nan, hist_trans_std)
+    hist_trans_std = hist_trans_std.T
+
+    #compressive
+    hist_comp,x_edge, y_edge, _ = stats.binned_statistic_2d(df[x_col].to_numpy(), df[y_col].to_numpy(), df[w_compressive].to_numpy(), statistic='mean', bins=[x_bin_edges, y_bin_edges])
+    hist_comp = np.where(np.isnan(hist_count_c), np.nan, hist_comp)
+    hist_comp = hist_comp.T
+
+    #compressive std
+    hist_comp_std,x_edge, y_edge, _ = stats.binned_statistic_2d(df[x_col].to_numpy(), df[y_col].to_numpy(), df[w_compressive].to_numpy(), statistic='std', bins=[x_bin_edges, y_bin_edges])
+    hist_comp_std = np.where(np.isnan(hist_count_c), np.nan, hist_comp_std)
+    hist_comp_std = hist_comp_std.T
     
-    return hist, hist_comp, hist_trans, hist_compressibility
+    #compressibility
+    hist_compressibility,x_edge, y_edge, _ = stats.binned_statistic_2d(df[x_col].to_numpy(), df[y_col].to_numpy(), df[w_compressible].to_numpy(), statistic='mean', bins=[x_bin_edges, y_bin_edges])
+    hist_compressibility = np.where(np.isnan(hist_count_c), np.nan, hist_compressibility)
+    hist_compressibility = hist_compressibility.T
+
+    hist_count = hist_count.T
+    print("results order is hist/transverse mean/trans std/comp mean/comp std/compressibility mean now! update me!")
+    return hist_count, hist_trans, hist_trans_std, hist_comp, hist_comp_std, hist_compressibility
 
 ##################################################################
 
